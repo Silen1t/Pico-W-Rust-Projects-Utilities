4157d9d60aa309f4f7a58b406bc3048ac8ac181f
Yousef Alsaleh <yousefalsal587@gmail.com>
2025-05-11 23:54:23 +0300

    Update Blinkyproject Cargo.toml
    Remove unused Crates

--- Projects/Blinky/src/main.rs
+++ Projects/Blinky/src/main.rs
@@ -1,7 +1,6 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::gpio::*;
 use embassy_time::Timer;
@@ -9,22 +8,17 @@ use ::{ defmt_rtt as _, panic_probe as _ };
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
+
     let p = embassy_rp::init(Default::default());
     let mut led = Output::new(p.PIN_15, Level::High);
     
-
     loop {
-        info!("led on!");
+        // led on!
         led.set_high();
         Timer::after_secs(1).await;
 
-        info!("led off!");
+        // led off!
         led.set_low();
         Timer::after_secs(1).await;
     }
-}
-
-#[embassy_executor::task]
-async fn test_task(){
-
 }
